5c9b4d7b81a9f94a60a3080f2b07132bff3a5a7b
adequateremedy <157491572+adequateremedy@users.noreply.github.com>
2026-06-29 23:40:01 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-.
+fetch('data/chapters.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log("CHAPTER DATA LOADED:");
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("ERROR LOADING CHAPTER DATA:", error);
+  });
