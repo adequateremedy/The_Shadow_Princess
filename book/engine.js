e1449a49ec23150227a037feab4112e186652304
adequateremedy <157491572+adequateremedy@users.noreply.github.com>
2026-06-30 08:52:19 -0500

    Create engine.js

---
+++ book/engine.js
@@ -0,0 +1,15 @@
+/*
+=========================================
+ The Shadow Princess Book Engine
+-----------------------------------------
+ This file coordinates the entire book.
+
+ It tells the other engine files when to:
+ - load chapters
+ - paginate text
+ - render pages
+ - play animations
+
+ It does NOT perform those jobs itself.
+=========================================
+*/
