60e774c2c3a6bbdfe4a59c2680ea1a946224c758
adequateremedy <157491572+adequateremedy@users.noreply.github.com>
2026-07-28 19:57:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const chapterMap = [
         { dir: "One", pages: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18] },
-        { dir: "Two", pages: [24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39] },
+        { dir: "Two", pages: [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39] },
         { dir: "Three", pages: Array.from({length: 20}, (_, i) => i + 40) }, 
         { dir: "Four", pages: [60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,75,77,78,79] }, 
         { dir: "Five", pages: Array.from({length: 25}, (_, i) => i + 80) }, 
